917c44e1df32630ffe97eccfbba9f27274373bcc
Ashish <ashishalex084@gmail.com>
2024-09-24 18:23:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,119 @@
+import React from 'react'
+import logo from './assets/Logo.png'
+import webDev from './assets/web-dev.png'
+import webDesign from './assets/web-design.png'
+import webContent from './assets/web-content.png'
+import portfolioImg1 from './assets/portfolio-1.png'
+import portfolioImg2 from './assets/portfolio-2.png'
+import portfolioImg3 from './assets/portfolio-3.png'
+import portfolioImg4 from './assets/portfolio-4.png'
+import TechStack1 from './assets/tech-stack-portfolio-1.png'
+import TechStack2 from './assets/tech-stack-portfolio-2.png'
+
+const App = () => {
+  return (
+    <>
+      <header className="hero">
+        <nav className="navbar">
+          <div className="container">
+            <div className="logo">
+              <img src={logo}alt="" />
+            </div>
+
+            <ul className="nav-links">
+              <li className="nav-link"><a className='hover-underline-animation' href="">Home</a></li>
+              <li className="nav-link"><a className='hover-underline-animation' href="">Services</a></li>
+              <li className="nav-link"><a className='hover-underline-animation' href="">Why Choose Us</a></li>
+              <li className="nav-link"><a className='hover-underline-animation' href="">Porfolio</a></li>
+            </ul>
+          </div>
+        </nav>
+
+        <div className="container">
+          <div className="hero-text">
+            <h1>Transforming <br></br>Web Ideas into<br></br> Reality</h1>
+            <p>We offer expert web development, design, and content<br></br> creation, building seamless, visually engaging websites that <br></br> elevate your online presence and drive meaningful results.</p>
+            <div className="cta">
+              <a className="button" href='#'>Let's Talk</a>
+              <a className="button" href='#'>Get a Quote</a>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <div className="background">
+        <div className="services">
+          <div className="container">
+            <div className="two-col">
+              <h1 className='heading'>Our<br></br> Services</h1>
+              <div className="service-cards">
+                <article className="service">
+                  <img src={webDev} alt="" />
+                  <div className="service-content">
+                    <h2 className='service-heading'>WEB DEVELOPMENT</h2>
+                    <p>Building robust, scalable websites tailored to your needs with the latest technologies for optimal performance.</p>
+                  </div>
+                </article>
+                <article className="service">
+                  <img src={webDesign} alt="" />
+                  <div className="service-content">
+                    <h2 className='service-heading'>WEB DESIGN</h2>
+                    <p>Crafting visually stunning, user-friendly designs that enhance user experience and brand identity.</p>
+                  </div>
+                </article>
+                <article className="service">
+                  <img src={webContent} alt="" />
+                  <div className="service-content">
+                    <h2 className='service-heading'>WEB CONTENT CREATION</h2>
+                    <p>Delivering compelling, SEO-optimized content that engages audiences and drives business growth.</p>
+                  </div>
+                </article>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="portfolio">
+          <div className="container">
+            <h1 className="heading">OUR RECENT PROJECTS</h1>
+            <div className="portfolio-grid">
+              <div className="portfolio-card">
+                <img className='portfolio-image' src={portfolioImg2} alt="" />
+                <div className="portfolio-card-content">
+                  <h1>CSI CATHEDRAL KOLLAM</h1>
+                  <img src={TechStack1} alt="" />
+                  <a href="#">Live Site</a>
+                </div>
+              </div>
+              <div className="portfolio-card">
+                <img  className='portfolio-image' src={portfolioImg3} alt="" />
+                <div className="portfolio-card-content">
+                  <h1>MIDTOWN RESTAURANT</h1>
+                  <img className='techStack2' src={TechStack2} alt="" />
+                  <a href="#" >Live Site</a>
+                </div>
+              </div>
+              <div className="portfolio-card">
+                <img className='portfolio-image' src={portfolioImg1} alt="" />
+                <div className="portfolio-card-content">
+                  <h1>NINESTARS</h1>
+                  <img src={TechStack1} alt="" />
+                  <a href="#">Live Site</a>
+                </div>
+              </div>
+              <div className="portfolio-card">
+                <img className='portfolio-image' src={portfolioImg4} alt="" />
+                <div className="portfolio-card-content">
+                  <h1>GENIUS GEMS ACADEMY</h1>
+                  <img src={TechStack1} alt="" />
+                  <a href="#">Live Site</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
